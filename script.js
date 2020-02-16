5d4e1f0dd038ce5d8bdce03c6bccffbdff0d084c
Sam23599 <61102596+Sam23599@users.noreply.github.com>
2020-02-16 10:15:57 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+console.log("Script working.");
+
+function runClick(){
+            console.log("Click function is working."); 
+            var a=new Date(document.getElementById("Date1").value);
+            var b=new Date(document.getElementById("Date2").value);
+            var c=Date.parse(b);
+            var d=Date.parse(a);
+            var diff=Math.abs(c-d);
+            diff=diff/1000;
+            console.log(diff);
+            document.getElementById("output").innerHTML=(diff+" seconds");
+                document.getElementById("output2").innerHTML=(diff*66000*0.2+" grams");
+        }
